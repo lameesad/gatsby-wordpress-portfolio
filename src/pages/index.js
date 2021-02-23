9db0e77fa7f9369af6375985f2b29fd266e7d7e8
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-02-23 12:00:59 +0200

    get data using graphql

---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import { graphql, StaticQuery } from "gatsby"
+
+const IndexPage = () => (
+  <Layout>
+    <StaticQuery
+      query={graphql`
+        {
+          allWordpressPage {
+            edges {
+              node {
+                id
+                title
+                content
+              }
+            }
+          }
+        }
+      `}
+      render={props => (
+        <div>
+          {props.allWordpressPage.edges.map(page => (
+            <div key={page.node.id}>
+              <h1>{page.node.title}</h1>
+              <div
+                dangerouslySetInnerHTML={{ __html: page.node.content }}
+              ></div>
+            </div>
+          ))}
+        </div>
+      )}
+    />
+  </Layout>
+)
+
+export default IndexPage
